5da0c69cf517eadd727069587d52e8db4aeb0825
Manolis Tsilikidis <manolistsilikidis@gmail.com>
2024-02-06 19:56:25 +0200

    Push failed test

--- test/marem_core_test.rb
+++ test/marem_core_test.rb
@@ -1,7 +1,8 @@
-require "test_helper"
+require 'test_helper'
 
 class MaremCoreTest < ActiveSupport::TestCase
-  test "it has a version number" do
+  test 'it has a version number' do
     assert MaremCore::VERSION
+    assert 1 == 2
   end
 end
